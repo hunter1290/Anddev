96526d0a219914d9cd57762177ea0d30103b3a3d
hunter1290 <79136754+hunter1290@users.noreply.github.com>
2021-10-06 12:31:24 +0530

    Add files via upload

---
+++ Python/filewritingatext.py
@@ -0,0 +1,7 @@
+# f = open("Tushar.txt","w")
+# f.write("Tushar is a good boy") #if we print a where a = f.write("Tushar is a good boy")
+# f.close()
+#---------------------------------------------#
+f=open("random.txt","r+")
+print(f.read())
+f.write( " "+"Thank You")
---
+++ Python/fstring.py
@@ -0,0 +1,9 @@
+#f string HELP US TO PRINT A THING IN A SRTING
+me="Console"
+# a="This is Tushar PC"%
+a1=3
+a="This is {} {} "
+b=a.format(me ,a1)
+print(b)
+A="LALALA"
+print(f"When everyone sing i  {A}")#best use of fstring
